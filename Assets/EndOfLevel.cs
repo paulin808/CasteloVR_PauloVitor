6312cc9706bbb508b892265de4acd003ba5443fe
paulin808 <pvtv61759@gmail.com>
2021-03-04 01:32:55 -0300

    apk completo

---
+++ Assets/EndOfLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndOfLevel : MonoBehaviour
+{
+    
+    public void endOfGame()
+    {
+        //Debug.Log("HIT");
+        Application.Quit();
+        SceneManager.LoadScene("fps");
+    }
+}
